f58d90bde90de487da87ada2259c7bb73e17dbe2
BortoBoy <gbortolote@gmail.com>
2023-04-14 13:46:06 -0300

    :art: Convert square css to styled-components logic

--- src/square.js
+++ src/square.js
@@ -1,10 +1,34 @@
-import './styles/square.css'
+//third
+import styled from 'styled-components'
+
+const SquareButton = styled.button`
+    background: #fff;
+    border: 1px solid #999;
+    float: left;
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 34px;
+    height: 34px;
+    margin-right: -1px;
+    margin-top: -1px;
+    padding: 0;
+    text-align: center;
+    width: 34px;
+
+    &:focus {
+        outline: none;
+    }
+
+    &.Highlight {
+        color: red;
+    }
+`
 
 export default function Square(props){
-    return <button
-        className={"Square" + (props.highlight ? " Highlight" : "")}
+    return <SquareButton
+        className={(props.highlight ? "Highlight" : "")}
         onClick={props.onClick}
     >
         {props.value}
-    </button>
+    </SquareButton>
 }
